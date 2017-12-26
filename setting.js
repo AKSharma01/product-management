ed13df49dafa156dc0e88c482854f46c314465ff
Akash <akashkr.sharma36@gmail.com>
2017-12-26 16:50:21 +0530

    minor change

--- setting.js
+++ setting.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongodb connection using mongoose
-mongoose.connect('mongodb://127.0.0.1/category');
+mongoose.connect('mongodb://127.0.0.1/product_management');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
